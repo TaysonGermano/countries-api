4f1e4f70ed4ef34e4cd9bed61594740c10730dca
Tyson Monteiro <taysongermano@gmail.com>
2022-06-13 10:36:25 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
       <header style={loading ? { display: "none" } : { display: "block" }}>
         <Navbar />
       </header>
-      <Countries loading={loading} />
+      <Countries/>
     </div>
   );
 }
